7ff53e8131f1a66b9e81619a0ef92ce6beba03f1
Gautham Ilango <gauthamilango@gmail.com>
2014-12-29 11:12:57 +0530

    Initial Commit

---
+++ Interview App/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Interview App
+//
+//  Created by Gautham Ilango on 29/12/14.
+//  Copyright (c) 2014 Gautham Ilango. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Interview App/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Interview App
+//
+//  Created by Gautham Ilango on 29/12/14.
+//  Copyright (c) 2014 Gautham Ilango. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
